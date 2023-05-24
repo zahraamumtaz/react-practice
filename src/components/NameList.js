feb99042c76be420b9916ec35322daa6f1522375
zahraamumtaz <zahra.mumtaz@invozone.com>
2023-05-24 17:35:43 +0500

    listing with key and index

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,25 +2,37 @@ import React from "react";
 import Persons from "./Persons";
 
 function NameList() {
-  const persons = [
-    {
-      name: "Bruce",
-      age: 32,
-      skill: "React.Js",
-    },
-    {
-      name: "Clark",
-      age: 30,
-      skill: "Angular",
-    },
-    {
-      name: "Diana",
-      age: 28,
-      skill: "Vue.Js",
-    },
-  ];
-  const personList = persons.map((person) => <Persons person={person} />);
+  const names = ["Clark", "Diana", "Bruce"];
+  const personList = names.map((name, index) => (
+    <h1 key={index}>
+      {index}: {name}
+    </h1>
+  ));
   return <div>{personList}</div>;
+  //   const persons = [
+  //     {
+  //       id: "1",
+  //       name: "Bruce",
+  //       age: 32,
+  //       skill: "React.Js",
+  //     },
+  //     {
+  //       id: "2",
+  //       name: "Clark",
+  //       age: 30,
+  //       skill: "Angular",
+  //     },
+  //     {
+  //       id: "3",
+  //       name: "Diana",
+  //       age: 28,
+  //       skill: "Vue.Js",
+  //     },
+  //   ];
+  //   const personList = persons.map((person) => (
+  //     <Persons key={person.id} person={person} />
+  //   ));
+  //   return <div>{personList}</div>;
 }
 
 export default NameList;
